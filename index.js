9868f87dbffc914cee106ae0ff7aa56a0c7a22b4
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-10-30 19:49:47 +0100

    added startGame function

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
 let firstCard = 11;
-let secondCard = 11;
+let secondCard = 10;
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
 
-if(sum <= 20) {
-    console.log("Do you want to draw a new card? 🙂");
-}
-else if(sum === 21){
-    console.log("Wohoo! You've got Blackjack! 🥳")
-    hasBlackJack = true;
-}
-else {
-    console.log("You're out of the game! 😭");
-    isAlive = false;
+let message = " ";
+
+let messageEl = document.getElementById('message-el');
+let sumEl = document.querySelector('#sum-el');
+let cardEl = document.getElementById('card-el');
+console.log(cardEl)
+
+function startGame() {
+    cardEl.textContent ="Cards: " + firstCard + " and " + secondCard;
+    sumEl.textContent = 'Sum: ' + sum;
+    if(sum <= 20) {
+        message = "Do you want to draw a new card? ";
+    }
+    else if(sum === 21){
+        message = "You've got Blackjack! ";
+        hasBlackJack = true;
+    }
+    else {
+        message = "You're out of the game! ";
+        isAlive = false;
+    }
+
+    messageEl.textContent = message;
 }
 
-console.log(isAlive)
+startGame();
+
 
