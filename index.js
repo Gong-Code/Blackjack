ef2a8a99fc7c237cf0af46a6391499333bdeaa2f
Gong Moonphruk <gong.moonphruk@gmail.com>
2023-11-01 18:52:24 +0100

    white spaces

--- index.js
+++ index.js
@@ -4,15 +4,11 @@ let cards = [firstCard, secondCard];
 let sum = firstCard + secondCard;
 let hasBlackJack = false;
 let isAlive = true;
-
-
 let message = " ";
 
 let messageEl = document.getElementById('message-el');
 let sumEl = document.querySelector('#sum-el');
 let cardEl = document.getElementById('card-el');
-// console.log(cardEl)
-
 
 function startGame() {
     renderGame();
